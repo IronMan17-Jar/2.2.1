d0bbe6b7aba5aad88a713d54f2d8a36f09d3eeb3
IronMan17-Jar <danil1704@mail.ru>
2021-12-09 19:45:20 +0300

    2.2.1

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,38 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User5", "lastname5", "user5@mail.ru", new Car("Toyota", 1)));
+      userService.add(new User("User6", "lastname6", "user6@mail.ru", new Car("BMW", 2)));
+      userService.add(new User("User7", "lastname7", "user7@mail.ru", new Car("Mercedes", 3)));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println(user.getCar());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
